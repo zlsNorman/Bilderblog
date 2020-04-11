3c53f28754ec008ed8a26930edd1abd5abea6492
zlsNorman <63201553+zlsNorman@users.noreply.github.com>
2020-04-11 21:37:29 +0200

    erste änderungen Startseite

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React from "react";
+import styles from '../../styles/index.module.scss';
 
 const Header = ({ name, anzahl, jahr }) => {
   return(
   <>
-    <h1>Ich bin der Header {name}</h1>
+    <div className={styles.headerStyle}> <h1>Jannis Art Gallery</h1></div>
+    <div className={styles.headerStyle}> <p>Startseite</p></div>
   </>
 )}
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import { Link } from "gatsby"
 import Button from "../components/button/button";
 import styles from '../styles/index.module.scss';
+import art from "../img/art.jpeg"
+import references from "../img/references.jpg"
 export default () => {
   const [counter, setCounter] = useState(12);
   const [name, setName] = useState('Ronny');
@@ -15,43 +17,25 @@ export default () => {
 
  return (
   <div>
-    <h1>startseite</h1>
-    <h2>Counter: {counter}</h2>
-    <h1>{name}</h1>
-    <Button handleClick={increaceCounter} name="+"/>
-    <Button handleClick={decreaseCounter} name="-"/>
-    <input onBlur={(e) => setName(e.target.value)} type="text"/>
-    <Link to="/test" name={name}>Testseite</Link>
+      {/*<h1>startseite</h1>
+      <h2>Counter: {counter}</h2>
+      <h1>{name}</h1>
+      <Button handleClick={increaceCounter} name="+"/>
+      <Button handleClick={decreaseCounter} name="-"/>
+      <input onBlur={(e) => setName(e.target.value)} type="text"/>
+      <Link to="/test" name={name}>Testseite</Link>
+      */}
+
     <div className={styles.imageGallery}>
-      <div>
-        <h1>Bild 1</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
+      <div className={styles.GalleryImage}>
+        <img src={art} alt="art"/>
+        <div className={styles.GalleryImageText}>
+          <h1>Art</h1>
+        </div>
       </div>
       <div>
-        <h1>Bild 2</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
-      </div>
-      <div>
-        <h1>Bild 3</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
-      </div>
-      <div>
-        <h1>Bild 4</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
-      </div>
-      <div>
-        <h1>Bild 5</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
-      </div>
-      <div>
-        <h1>Bild 6</h1>
-        <img src="https://i.picsum.photos/id/48/200/300.jpg"/>
-        <p>lorem ipsduzfshdfhnsdfhshdf hsdhfhsdhfhsdfh.</p>
+        <h1>References</h1>
+        <img src={references} alt="references"/>
       </div>
     </div>
   </div>
